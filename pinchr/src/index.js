c6a40227e0f5736a5c60b50781a04711de2d9aac
andrewcesario1 <63276479+andrewcesario1@users.noreply.github.com>
2024-04-01 16:45:10 -0700

    Update index.js

--- pinchr/src/index.js
+++ pinchr/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Login';
 import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
