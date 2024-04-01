32c5cb551fd9c7ae398eee26ccf177c96d24c598
andrewcesario1 <63276479+andrewcesario1@users.noreply.github.com>
2024-04-01 16:25:30 -0700

    Changed file structure to match react app
    folder node_modules is included in gitignore, but needed to run the react app. If node.js is installed inside a terminal in the projects directory type: npm install
    
    to start the react app type in the terminal of project directory: npm run start

---
+++ pinchr/src/Login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Login.css'; 
+
+const Login = () => {
+  return (
+    <div>
+      <div className="header">
+        <h1>Pinchr</h1>
+      </div>
+      <div className="Login">
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" placeholder="Enter Username" required /><br /><br />
+        <label htmlFor="pass">Password:</label>
+        <input type="password" id="pass" placeholder="Enter Password" required /><br /><br />
+        <button id="loginbtn">Login</button><br /><br />
+        <a href="#" id="register">Don't have an account? Register here.</a>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
